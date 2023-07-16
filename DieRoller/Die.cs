d2e830739b5208b24bb779df2864f57f02d4b593
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-07-16 15:36:46 -0700

    Added basic structure of die class

---
+++ DieRoller/Die.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieRoller
+{
+    /// <summary>
+    /// Represents a single six sided die (1 - 6)
+    /// </summary>
+    public class Die
+    {
+        byte FaceValue { get; set; }
+        bool IsHeld { get; set; }
+
+        public Die() {
+        }
+
+        public int Roll()
+        {
+            Random rnd = new Random();
+            FaceValue = rnd.Next(1, 7);
+            return FaceValue;
+        }
+    }
+}
